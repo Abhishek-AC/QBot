b37b69ba4d1aa330cbe875ced17aa3f71fb13eba
Abhishek Chhabra <02abhishek.chhabra@gmail.com>
2021-05-14 08:54:56 -0700

    goof up

--- Source/DashboardTabApp/src/environments/environment.ts
+++ Source/DashboardTabApp/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://qbot_test_ipodia.azurewebsites.net/api/Request/",
+    apiBaseUrl: "https://qbot-test-ipodia.azurewebsites.net/api/Request/",
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
